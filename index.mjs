b6c055a38130fef81e1124992340f47c8d1396ad
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-10 23:42:54 +0100

    Reformat code

--- index.mjs
+++ index.mjs
@@ -1,30 +1,30 @@
-import { Extension, Readme, Service } from "talkops";
+import { Extension, Readme, Service } from 'talkops'
 
-const extension = new Extension("Template NodeJS");
+const extension = new Extension('Template NodeJS')
 
-extension.setDockerRepository("ghcr.io/talkops/talkops-extension-template-nodejs");
+extension.setDockerRepository('ghcr.io/talkops/talkops-extension-template-nodejs')
 
 extension.setDescription(`
 This Extension serves as a template designed to assist Node.js developers in effortlessly creating and integrating their own extensions.
-`);
+`)
 
 extension.setInstructions(`
 You are an intelligent and efficient note-taking assistant.
 Your role is to help users capture, organize, and refine their notes effectively.
 Provide clear, concise, and structured responses while ensuring readability and usability.
 Adapt to the user's style and preferences to enhance their note-taking experience.
-`);
+`)
 
-import save_note from "schemas/functions/save_note.json" with { type: "json" };
+import save_note from 'schemas/functions/save_note.json' with { type: 'json' }
 
-extension.setFunctionSchemas([save_note]);
+extension.setFunctionSchemas([save_note])
 
 extension.setFunctions([
   function save_note(note) {
-    console.log(`Do something with: ${note}`);
-    return "Done.";
+    console.log(`Do something with: ${note}`)
+    return 'Done.'
   },
-]);
+])
 
-new Readme(process.env.README_TEMPLATE_URL, "/app/README.md", extension);
-new Service(process.env.AGENT_URLS.split(","), extension);
+new Readme(process.env.README_TEMPLATE_URL, '/app/README.md', extension)
+new Service(process.env.AGENT_URLS.split(','), extension)
